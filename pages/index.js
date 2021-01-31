85637e39ae85d8d903dad70a4eecb5e703f0f762
Bateristico <camilo.saa@gmail.com>
2021-01-31 19:45:58 +0100

    5. Layout component

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <Navbar /> 
       <h1>Homepage</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto consequatur quod vel iste at fugiat minima commodi perferendis, doloremque earum, dicta vitae, neque porro temporibus culpa. Consequuntur exercitationem aperiam sapiente!</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto consequatur quod vel iste at fugiat minima commodi perferendis, doloremque earum, dicta vitae, neque porro temporibus culpa. Consequuntur exercitationem aperiam sapiente!</p>
         <Link href="/ninjas">
           <a>See Ninja Listing</a>
         </Link>
-        <Footer />
     </div>
   )
 }
