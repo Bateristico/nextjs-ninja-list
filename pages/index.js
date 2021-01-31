c46c831e736a411d682d0b66827fed670327549c
Bateristico <camilo.saa@gmail.com>
2021-01-31 19:16:02 +0100

    3. Adding some components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <div>
+      <Navbar /> 
       <h1>Homepage</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto consequatur quod vel iste at fugiat minima commodi perferendis, doloremque earum, dicta vitae, neque porro temporibus culpa. Consequuntur exercitationem aperiam sapiente!</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto consequatur quod vel iste at fugiat minima commodi perferendis, doloremque earum, dicta vitae, neque porro temporibus culpa. Consequuntur exercitationem aperiam sapiente!</p>
+        <Footer />
     </div>
   )
 }
