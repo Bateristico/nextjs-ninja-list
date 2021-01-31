143aabf6a819e00d1c19ac6a7e97fa6293532609
Bateristico <camilo.saa@gmail.com>
2021-01-31 19:39:01 +0100

    4. Linking between pages

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       <h1>Homepage</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto consequatur quod vel iste at fugiat minima commodi perferendis, doloremque earum, dicta vitae, neque porro temporibus culpa. Consequuntur exercitationem aperiam sapiente!</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto consequatur quod vel iste at fugiat minima commodi perferendis, doloremque earum, dicta vitae, neque porro temporibus culpa. Consequuntur exercitationem aperiam sapiente!</p>
+        <Link href="/ninjas">
+          <a>See Ninja Listing</a>
+        </Link>
         <Footer />
     </div>
   )
