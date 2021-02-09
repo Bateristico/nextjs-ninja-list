feeb6f56c371b07a918d316bfc9e56757a81aac9
Bateristico <camilo.saa@gmail.com>
2021-02-09 09:36:05 +0100

    13.  Fetching a single item

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,7 +1,8 @@
-export const getStaticPath = async () => {
+export const getStaticPaths = async () => {
 	const res = await fetch('https://jsonplaceholder.typicode.com/users');
 	const data = await res.json();
 
+	// map data to an array of path objects with params (id)
 	const paths = data.map((ninja) => {
 		return {
 			params: { id: ninja.id.toString() },
@@ -14,10 +15,23 @@ export const getStaticPath = async () => {
 	};
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+	const id = context.params.id;
+	const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+	const data = await res.json();
+
+	return {
+		props: { ninja: data },
+	};
+};
+
+const Details = ({ ninja }) => {
 	return (
 		<div>
-			<h1>Details page</h1>
+			<h1>{ninja.name}</h1>
+			<p>{ninja.email}</p>
+			<p>{ninja.website}</p>
+			<p>{ninja.address.city}</p>
 		</div>
 	);
 };
